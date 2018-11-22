30db2f9dd276a9c2cf9d162d0e3d2300e3fbef08
yanghanchao <695386282@qq.com>
2018-11-22 16:29:55 +0800

    init

---
+++ src/components/home/BlogIndex.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <blog-header></blog-header>
+    <hr/>
+    <div>
+      这是首页！！
+    </div>
+    <hr/>
+    <blog-footer></blog-footer>
+  </div>
+</template>
+
+<script>
+import blogHeader from '@/components/common/BlogHeader.vue'
+import blogFooter from '@/components/common/BlogFooter.vue'
+
+export default {
+  name: 'BlogIndex',
+  components: { blogFooter, blogHeader }
+}
+</script>
